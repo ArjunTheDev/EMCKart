18e8bc9712e4c858d8558c8cff57e9e01a0176c0
ArjunTheDev <arjunthedev@gmail.com>
2023-12-31 09:38:57 +0530

    Updated config and env
    Updated config and env

--- EMC-Kart-Api/config/db.config.js
+++ EMC-Kart-Api/config/db.config.js
@@ -1,10 +1,5 @@
 require('dotenv').config();
 
-// module.exports = {
-//     url: 'mongodb://'+ process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + 
-//         '@localhost:27017/' + process.env.DB_NAME
-//   };
-
 module.exports = {
-    url: 'mongodb://localhost:27017/EMCKart'
+    url: `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`
 };
